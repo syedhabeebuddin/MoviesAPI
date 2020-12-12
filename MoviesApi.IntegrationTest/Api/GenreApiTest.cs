1068da5e9086e25bdaea6fcad7bdd790ea3ef294
syedhabeebuddin <syedhabeebuddin0@gmail.com>
2020-12-13 00:21:20 +0530

    Added IntegrationTests

---
+++ MoviesApi.IntegrationTest/Api/GenreApiTest.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using MoviesApi.Core.Configurations;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MoviesApi.IntegrationTest.Api
+{
+    public class GenreApiTest
+    {
+        private readonly HttpClient _client;
+
+        public GenreApiTest()
+        {
+            var config = new ConfigurationBuilder()
+                             .SetBasePath(Directory.GetCurrentDirectory())
+                             .AddJsonFile("appsettings.json", optional: true)            
+                             .Build();
+
+            var server = new TestServer(new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseConfiguration(config)                
+                .UseStartup<Startup>()); ;
+            _client = server.CreateClient();
+        }
+
+        //[Theory]
+        //[InlineData("GET")]
+        //public async Task GenreGetAllTestAsync(string method)
+        //{
+        //    // Arrange
+        //    var request = new HttpRequestMessage(new HttpMethod(method), "/api/Genres/");
+
+        //    // Act
+        //    var response = await _client.SendAsync(request);
+
+        //    // Assert
+        //    response.EnsureSuccessStatusCode();
+        //    Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        //}
+
+        [Theory]
+        [InlineData("GET", "5fd48668e2227ef25e47a59b")]
+        public async Task GenreGetByIdTestAsync(string method,string id)
+        {
+            // Arrange
+            var request = new HttpRequestMessage(new HttpMethod(method), $"/api/Genres/{id}");
+
+            // Act
+            var response = await _client.SendAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ MoviesApi.IntegrationTest/Api/MovieApiTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MoviesApi.IntegrationTest.Api
+{
+    public class MovieApiTest
+    {
+        private readonly HttpClient _client;
+
+        public MovieApiTest()
+        {
+            var config = new ConfigurationBuilder()
+                             .SetBasePath(Directory.GetCurrentDirectory())
+                             .AddJsonFile("appsettings.json", optional: true)
+                             .Build();
+
+            var server = new TestServer(new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseConfiguration(config)
+                .UseStartup<Startup>()); ;
+            _client = server.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("DELETE", "5fd4ba7cef6928616c200a6c")]
+        [InlineData("DELETE", "5fd4bab4ef6928616c200a6d")]
+        public async Task MovieDeleteTestAsync(string method, string id)
+        {
+            // Arrange
+            var request = new HttpRequestMessage(new HttpMethod(method), $"/api/Movies/{id}/Delete");
+
+            // Act
+            var response = await _client.SendAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
